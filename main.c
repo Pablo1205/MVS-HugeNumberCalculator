0e29c1e561314906ee50d7013072b3d11eac4391
Pablo1205 <64144292+Pablo1205@users.noreply.github.com>
2020-11-11 14:50:28 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (){
-  printf("Hello World !\n");
+int main (void) {
+  HugeUnsignedInt* hugeNumber = createHugeUnsignedIntFromString("123456789012345678901234567890");
+  printHugeUnsignedInt (hugeNumber);
+  hugeNumber = createHugeUnsignedIntFromString("0000000123456789012345678901234567890");
+  printHugeUnsignedInt (hugeNumber);
 }
